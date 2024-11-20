880b1946745506d1a84fec4e911488a0d97c1d40
Mathilde Ballouhey <m.ballouhey@gmail.com>
2024-11-21 00:10:49 +0100

    fix: DateFormatter month error

--- utils/__tests__/DateFormatter-test.ts
+++ utils/__tests__/DateFormatter-test.ts
@@ -1,7 +1,7 @@
 import DateFormatter from "../DateFormatter";
 
 test("Correctly formats date", () => {
-    expect(DateFormatter.formatDate(new Date(2022, 2, 1))).toBe("2022-02-01");
+    expect(DateFormatter.formatDate(new Date(2022, 1, 1))).toBe("2022-02-01");
 });
 
-export default {}
+export default {};
